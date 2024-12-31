86d677116c61de0cd3a533e166975403ec86bfb6
Lone-Arjun <121416797+Lone-Arjun@users.noreply.github.com>
2024-12-31 17:10:47 +0545

    Update overview.tsx

--- components/stats/overview.tsx
+++ components/stats/overview.tsx
@@ -1,4 +1,4 @@
-import { Users, UserMinus, Groups, Filter } from "lucide-react";
+import { Users, UserMinus, Group, Filter } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { StatsCard } from "./stats-card";
